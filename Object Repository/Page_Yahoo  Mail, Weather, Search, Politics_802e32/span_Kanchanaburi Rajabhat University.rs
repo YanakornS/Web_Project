1f3b30a59fbf088a7f81eeb44eae494da86c289b
YanakornS <654259002@webmail.npru.ac.th>
2024-01-16 15:51:50 +0700

    first commit

---
+++ Object Repository/Page_Yahoo  Mail, Weather, Search, Politics_802e32/span_Kanchanaburi Rajabhat University.rs
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Kanchanaburi Rajabhat University</name>
+   <tag></tag>
+   <elementGuidId>36df40c5-3ab4-4de2-9587-0d515f02f354</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>span._yb_xr24s</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//form[@id='ybar-sf']/div/div/div/ul/li/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>30906dfe-afd7-4f4e-9548-122922e91284</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>_yb_xr24s</value>
+      <webElementGuid>6af086ca-de45-4075-bfe4-690c60da2d54</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Kanchanaburi Rajabhat University</value>
+      <webElementGuid>0c34086b-4ed7-45a4-9793-5b8c22efca23</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;ybar-sf&quot;)/div[@class=&quot;_yb_w6xyq&quot;]/div[@class=&quot;_yb_1igqf _yb_1r4bd&quot;]/div[@class=&quot;_yb_1nfum&quot;]/ul[@class=&quot;_yb_1ws6q&quot;]/li[@class=&quot;_yb_xe8nk _yb_1jdye _yb_1s9ii _yb_1fy5d&quot;]/span[@class=&quot;_yb_xr24s&quot;]</value>
+      <webElementGuid>f6609cb9-814b-4aa6-90c5-5060318bf517</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='ybar-sf']/div/div/div/ul/li/span</value>
+      <webElementGuid>53f0d6c9-ad4a-4a48-a8f4-cfe860bac840</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/ul/li/span</value>
+      <webElementGuid>2d56af47-efad-4b14-bc35-d5ca65100273</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'Kanchanaburi Rajabhat University' or . = 'Kanchanaburi Rajabhat University')]</value>
+      <webElementGuid>f039dadb-88a6-4dca-ad9d-2e0f4bf431b5</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/TSC01-Search By Keyword.ts
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>TSC01-Search By Keyword</name>
+   <tag></tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations/>
+</TestSuiteCollectionEntity>
